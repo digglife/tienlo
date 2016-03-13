fca1c02a7bbe7d82b67c99311924a32c28ce05d1
SHENG LI ZHU <zshengli@cn.ibm.com>
2016-03-13 10:28:13 +0800

    add xunlei remote

--- crawler.pl
+++ crawler.pl
@@ -12,10 +12,15 @@ use DBI;
 use DBD::SQLite;
 use Cwd qw/abs_path getcwd/;
 use File::Spec;
+use WWW::Xunlei;
 
-#use WWW::Xunlei;
+my $site   = "http://www.mp4ba.com";
+my $config = get_config('config.json');
 
-my $site = "http://www.mp4ba.com";
+my $xunlei = WWW::Xunlei->new( $config->{'xunlei'}->{'username'},
+    $config->{'xunlei'}->{'password'} );
+
+my $downloader = $xunlei->list_downloaders()->[0];
 
 my $dbh = get_dbh('scroll.db');
 my @last_record
@@ -29,14 +34,17 @@ my $last_movie_title = $last_record[0];
 my $ua = LWP::UserAgent->new;
 $ua->agent( 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:41.0)'
         . ' Gecko/20100101 Firefox/41.0' );
+
 $ua->timeout(180);
 my $response = $ua->get($site);
-die "$site is not available now" unless $response->is_success;
+die "$site is not available now : " . $response->code
+    unless $response->is_success;
 
 my $tree = HTML::TreeBuilder->new;
-# $tree->parse( $response->content );
 
-$tree->parse_file('mp4ba.html');
+$tree->parse( $response->content );
+
+# $tree->parse_file('mp4ba.html');
 
 my $table = $tree->find_by_attribute( 'id', 'data_list' );
 my @rows;
@@ -44,12 +52,13 @@ my @rows;
 for my $tr ( $table->look_down( '_tag', 'tr', sub { !$_[0]->attr('id') } ) ) {
     my @cols;
 
-    # Acutally only the third td is valuable for me, but still loop all the tds
-    # for future use.
+   # Acutally only the third td is valuable for me, but still loop all the tds
+   # for future use.
     for my $td ( $tr->find_by_tag_name('td') ) {
         my $text = $td->as_text;
         if ( $td->attr('style') ) {
             my $a = $td->look_down( '_tag', 'a' );
+
             # my ( $hash ) = $a->attr('href') =~ /=(.*)$/;
             # push @cols, $hash;
             push @cols, $site . '/' . $a->attr('href');
@@ -61,9 +70,11 @@ for my $tr ( $table->look_down( '_tag', 'tr', sub { !$_[0]->attr('id') } ) ) {
 
     print "Processing " . $cols[3] . "\n";
     last if $last_movie_title && $last_movie_title eq $cols[3];
-    my @status = 
-        $dbh->selectrow_array("SELECT is_downloaded from movies "
-            . "WHERE page_title = '" . $cols[3] . "'" );
+    my @status
+        = $dbh->selectrow_array( "SELECT is_downloaded from movies "
+            . "WHERE page_title = '"
+            . $cols[3]
+            . "'" );
     my $is_downloaded = $status[0] ? 1 : 0;
     next if $is_downloaded;
     my ($hash) = $cols[2] =~ /=(.*)$/;
@@ -72,17 +83,18 @@ for my $tr ( $table->look_down( '_tag', 'tr', sub { !$_[0]->attr('id') } ) ) {
         . $hash
         . "&tr=http://bt.mp4ba.com:2710/announce";
     my ( $title, $quality )
-        = $cols[3]
-        =~ /(.*?)\..*((?:BD|HD|TS|TC)(?:720|1080)P|DVDRIP|DVDSRC)/;
+        = $cols[3] =~ /(.*?)\..*((?:BD|HD|TS|TC)(?:720|1080)P|DVDRIP|DVDSRC)/;
     print "Real Title => $title\n";
-    my $rating        = get_douban_rating($title);
+    my $rating = get_douban_rating($title);
     print "RATING => $rating\n";
 
-    if ( $rating > 7 && $quality =~ /(BD|HD)720p/i ) {
-        #$status = $downloader->create_task($download_url);
-        print "Virtual Download The Following One:\n"
-            . "$title -> ( Rating: $rating ; Quality: $quality )\n";
-        
+    if (   $rating > $config{'rating'}
+        && $quality =~ /(BD|HD)$config{'quality'}/i )
+    {
+        eval { $downloader->create_task($download_url); };
+
+        # print "Virtual Download The Following One:\n"
+        #     . "$title -> ( Rating: $rating ; Quality: $quality )\n";
         if ($@) {
             next;
         }
@@ -91,10 +103,9 @@ for my $tr ( $table->look_down( '_tag', 'tr', sub { !$_[0]->attr('id') } ) ) {
         }
     }
 
-    @cols = ( @cols[3, 2], $download_url, $is_downloaded, $site );
+    @cols = ( @cols[ 3, 2 ], $download_url, $is_downloaded, $site );
     push @rows, \@cols;
 }
-
 
 #print Dumper($rows[0]);
 
@@ -104,7 +115,7 @@ my $sth
         . " VALUES (?, ?, ?, ?, ?)" );
 
 for ( my $i = $#rows; $i >= 0; $i-- ) {
-    $sth->execute( @{$rows[$i]} );
+    $sth->execute( @{ $rows[$i] } );
 }
 
 $sth->finish;
@@ -143,6 +154,7 @@ sub get_douban_rating {
         = "https://movie.douban.com/subject_search?search_text="
         . url_encode($title)
         . "&cat=1002";
+
     # my $ua = LWP::UserAgent->new;
     # $ua->agent( 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:41.0)'
     #         . ' Gecko/20100101 Firefox/41.0' );
@@ -160,3 +172,15 @@ sub send_notification {
     my ( $email, $message ) = @_;
 }
 
+sub get_config {
+    my $file_name = shift;
+
+    my $dir = abs_path(getcwd);
+    my $file = File::Spec->catfile( $dir, $file_name );
+
+    use JSON qw/decode_json/;
+    open( my $fh, '<', $file ) or die "Unable to open $file: $!";
+    my $content = join( '', <$fh> );
+    my $config = decode_json($content);
+    return $config;
+}
